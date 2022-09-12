99ddff1c1511b6ee89ccf5c6a02e8b9e751adfdb
Renato Tenório Lino <103604513+renatotl@users.noreply.github.com>
2022-09-12 18:53:51 -0300

    update

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,15 +3,16 @@ import "../Login/Login.css";
 export function Login() {
   return (
     <section className='page login'>
-    <form onSubmit= "">
-        <fieldset>
-          <input type="name" name="name" />
-            <input type="email" name={"inputEmail"} description={"Email"} />
-            <input type="password" name={"inputPassword"} description={"Password"}/>
-            <button description={"Quero Me Logar"} />
-          
+    <form className="form" onSubmit= "login">
+        <fieldset className="fieldset">
+          <input className="input" type="name" name="name" placeholder="name" />
+            <input className="input" type="usernName" name="userName" placeholder="user name"  />
+            <input className="input" type="email" name="email" placeholder="email"  />
+            <input className="input" type="password" name="password" placeholder="password"/>
+            <button className="B" type="button"  description={"Quero Me Logar"} />
         </fieldset>
     </form>
 </section >
+//
   );
 }
