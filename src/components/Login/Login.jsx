58d2334ef74b69b611b3b528bff7cb52d46d7735
Douglas Volcato <douglasvolcato@gmail.com>
2022-09-13 17:20:26 -0300

    Added api connection

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,23 +1,55 @@
+import { useState } from "react";
 import "../Login/Login.css";
+import { jogoRDJ } from "../utils/api/api";
 
 export function Login() {
+  const [user, setUser] = useState();
+
+  async function loginUser(event) {
+    event.preventDefault();
+
+    setUser({ ...user });
+
+    console.log(user);
+
+    const apiConfirmation = await jogoRDJ.login(user);
+
+    try {
+      if (apiConfirmation.message.includes("not found")) {
+        alert("Email not registered.");
+      } else if (apiConfirmation.message.includes("Invalid password")) {
+        alert("Invalid password.");
+      }
+    } catch (err) {
+      alert("Successfully logged in!");
+    }
+  }
+
   return (
     <section className="page login">
-      <form className="form" onSubmit="login">
+      <form className="form" onSubmit={loginUser}>
         <fieldset className="fieldset">
           <input
             className="input"
             type="email"
             name="email"
             placeholder="email"
+            required={true}
+            onChange={(event) => {
+              setUser({ ...user, email: event.target.value });
+            }}
           />
           <input
             className="input"
             type="password"
             name="password"
             placeholder="password"
+            required={true}
+            onChange={(event) => {
+              setUser({ ...user, password: event.target.value });
+            }}
           />
-          <button className="B" type="button">
+          <button className="B" type="submit">
             Send
           </button>
         </fieldset>
--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -1,18 +1,76 @@
+import { useState } from "react";
 import "../Registration/Registration.css";
+import { jogoRDJ } from "../utils/api/api";
 
 export function Registration() {
+  const [newUser, setNewUser] = useState();
+
+  async function registerUser(event) {
+    event.preventDefault();
+
+    setNewUser({ ...newUser });
+
+    const apiConfirmation = await jogoRDJ.registerUser(newUser);
+
+    try {
+      if (apiConfirmation.message.includes("Error")) {
+        alert("Email or username already registered, try a different one.");
+      }
+    } catch (err) {
+      alert("Successfully registered!");
+    }
+  }
+
   return (
-    <section className='Registration'>
-    <form className="form" onSubmit= "login">
+    <section className="Registration">
+      <form className="form" onSubmit={registerUser}>
         <fieldset className="fieldset">
-          <input className="input" type="name" name="name" placeholder="name" />
-            <input className="input" type="usernName" name="userName" placeholder="user name"  />
-            <input className="input" type="email" name="email" placeholder="email"  />
-            <input className="input" type="password" name="password" placeholder="password"/>
-            <button className="B" type="button" >Send</button  >
+          <input
+            className="input"
+            type="name"
+            name="name"
+            placeholder="name"
+            required={true}
+            onChange={(event) => {
+              setNewUser({ ...newUser, name: event.target.value });
+            }}
+          />
+          <input
+            className="input"
+            type="usernName"
+            name="userName"
+            placeholder="user name"
+            required={true}
+            onChange={(event) => {
+              setNewUser({ ...newUser, username: event.target.value });
+            }}
+          />
+          <input
+            className="input"
+            type="email"
+            name="email"
+            placeholder="email"
+            required={true}
+            onChange={(event) => {
+              setNewUser({ ...newUser, email: event.target.value });
+            }}
+          />
+          <input
+            className="input"
+            type="password"
+            name="password"
+            placeholder="password"
+            required={true}
+            onChange={(event) => {
+              setNewUser({ ...newUser, password: event.target.value });
+            }}
+          />
+          <button className="B" type="submit">
+            Send
+          </button>
         </fieldset>
-    </form>
-</section >
-//
+      </form>
+    </section>
+    //
   );
 }
