fabeb03d710038a543aa4c97fd02189a24037114
Douglas Volcato <douglasvolcato@gmail.com>
2022-09-13 16:39:51 -0300

    Fixed login page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,17 +2,27 @@ import "../Login/Login.css";
 
 export function Login() {
   return (
-    <section className='page login'>
-    <form className="form" onSubmit= "login">
+    <section className="page login">
+      <form className="form" onSubmit="login">
         <fieldset className="fieldset">
-          <input className="input" type="name" name="name" placeholder="name" />
-            <input className="input" type="usernName" name="userName" placeholder="user name"  />
-            <input className="input" type="email" name="email" placeholder="email"  />
-            <input className="input" type="password" name="password" placeholder="password"/>
-            <button className="B" type="button" >Enviar</button  >
+          <input
+            className="input"
+            type="email"
+            name="email"
+            placeholder="email"
+          />
+          <input
+            className="input"
+            type="password"
+            name="password"
+            placeholder="password"
+          />
+          <button className="B" type="button">
+            Send
+          </button>
         </fieldset>
-    </form>
-</section >
-//
+      </form>
+    </section>
+    //
   );
 }
